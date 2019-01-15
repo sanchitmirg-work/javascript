69e776a16f9f6767eebc88204e55f68d9b5ddd0e
Sanchit Mirg <sanchitm@Meditab.Local>
2019-01-15 20:17:04 +0530

    changes in js file

--- DOM-pig-game/starter/app.js
+++ DOM-pig-game/starter/app.js
@@ -11,16 +11,9 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     //Do something here
@@ -53,14 +46,18 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     //2. update the user interface
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 20) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner';
         document.querySelector('.dice').style.display = 'none';
+        document.querySelector('player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('player-' + activePlayer + '-panel').classList.remove('active');
     } else {
         nextPlayer();
     }
     //3. Check if player has won the game.
 })
+
+document.querySelector('.btn-new').addEventListener('click', init);
 
 function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
@@ -75,4 +72,27 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active');
 
     document.querySelector('.dice').style.display = 'none';
+}
+
+function init() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
+
+    document.querySelector('player-0-panel').classList.remove('winner');
+    document.querySelector('player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+
+    document.querySelector('.player-0-panel').classList.add('active');
 }
